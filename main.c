800d481b4f95c027956f28b4d2423338ec42085f
风的影子 <oncwnuHIi_u71WrUflnCwh-IBN2s@git.weixin.qq.com>
2020-07-30 14:49:26 +0800

    按下时进行绘制

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+int isDown = 0;
 int main(int argc, char *argv[])
 {
     SDL_Window *window=SDL_CreateWindow("demo",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,640,480,SDL_WINDOW_SHOWN);
@@ -19,10 +20,20 @@ int main(int argc, char *argv[])
                     quit=true;
                     break;
                 case SDL_MOUSEMOTION:
+                    if(isDown){
                     SDL_RenderDrawLine(renderer,point.x,point.y,event.motion.x,event.motion.y);
                     point.x=event.motion.x;
                     point.y=event.motion.y;
+                    }
                     break;
+                case SDL_MOUSEBUTTONDOWN:
+                    point.x=event.motion.x;
+                    point.y=event.motion.y;
+                    isDown = 1;
+                break;
+                case SDL_MOUSEBUTTONUP:
+                    isDown = 0;
+                break;
             }
         }
         SDL_RenderPresent(renderer);
